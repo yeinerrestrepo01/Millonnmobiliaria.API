6ef5e5f5bece6e0bfcb34dd120e2452f5874b87d
yeinerrestrepo01 <yeinermeri@gmail.com>
2021-12-17 21:11:56 -0500

    ajuste al nombre del campo name de la tabla owner

--- Millon.Inmobiliaria.Infrastructure/Migrations/20211218021112_Ajuste_Nombre_Campo_Name.Designer.cs
+++ Millon.Inmobiliaria.Infrastructure/Migrations/20211218021112_Ajuste_Nombre_Campo_Name.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Millon.Inmobiliaria.Infrastructure.DBContext;
 
 namespace Millon.Inmobiliaria.Infrastructure.Migrations
 {
     [DbContext(typeof(MillonInmobiliariaDbContext))]
-    partial class MillonInmobiliariaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211218021112_Ajuste_Nombre_Campo_Name")]
+    partial class Ajuste_Nombre_Campo_Name
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Millon.Inmobiliaria.Infrastructure/Migrations/20211218021112_Ajuste_Nombre_Campo_Name.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Millon.Inmobiliaria.Infrastructure.Migrations
+{
+    public partial class Ajuste_Nombre_Campo_Name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nambe",
+                table: "Owner",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Owner",
+                newName: "Nambe");
+        }
+    }
+}
