1438e40b8b6e009341358ec431e84fcf891c9d49
yeinerrestrepo01 <yeinermeri@gmail.com>
2021-12-17 21:48:55 -0500

    ajustes de reques owner

---
+++ Millon.Inmobiliaria.Domain/Request/OwnerResquest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Millon.Inmobiliaria.Domain.Request
+{
+    public class OwnerResquest
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Photo { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
