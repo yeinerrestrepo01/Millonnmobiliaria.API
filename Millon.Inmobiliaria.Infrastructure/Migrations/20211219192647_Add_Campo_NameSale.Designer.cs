0a8434ae6229d5aa1d76d01aca9c3e6aee3fc89b
yeinerrestrepo01 <yeinermeri@gmail.com>
2021-12-19 14:31:39 -0500

    se crea columna Name en tabala property  Trace

--- Millon.Inmobiliaria.Infrastructure/Migrations/20211219192647_Add_Campo_NameSale.Designer.cs
+++ Millon.Inmobiliaria.Infrastructure/Migrations/20211219192647_Add_Campo_NameSale.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Millon.Inmobiliaria.Infrastructure.DBContext;
 
 namespace Millon.Inmobiliaria.Infrastructure.Migrations
 {
     [DbContext(typeof(MillonInmobiliariaDbContext))]
-    partial class MillonInmobiliariaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211219192647_Add_Campo_NameSale")]
+    partial class Add_Campo_NameSale
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Millon.Inmobiliaria.Infrastructure/Migrations/20211219192647_Add_Campo_NameSale.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Millon.Inmobiliaria.Infrastructure.Migrations
+{
+    public partial class Add_Campo_NameSale : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PropertyTrace",
+                type: "varchar(200)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PropertyTrace");
+        }
+    }
+}
